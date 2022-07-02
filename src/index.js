58c14ccc7617d1468528e9ef00c785537969155b
Hans Henrik Grønsleth <hh.gronsleth@gmail.com>
2022-07-02 13:03:26 +0200

    Install polyfill for smooth scrolling for safety

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import dayjs from "dayjs";
 import "dayjs/locale/nb";
+import smoothscroll from "smoothscroll-polyfill";
 
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 dayjs.locale("nb");
+
+smoothscroll.polyfill();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
